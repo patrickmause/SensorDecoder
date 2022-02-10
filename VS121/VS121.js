18f43be4e8c65b3cd4948e791cdbe7e2cec8f906
Patrick Mause <patrick@morgenbt.com>
2022-02-10 12:18:15 -0500

    Update VS121.js

--- VS121/VS121.js
+++ VS121/VS121.js
@@ -7,10 +7,11 @@
  */
 function Decode(fPort, bytes) {
     var decoded = {};
+    var index = 3;
     decoded.devEUI = LoRaObject.devEUI;
     decoded.rssi = LoRaObject.rxInfo[0].rssi;
     decoded.deviceName = LoRaObject.deviceName;
-    
+
     for (i = 0; i < bytes.length; ) {
         var channel_id = bytes[i++];
         var channel_type = bytes[i++];
@@ -36,7 +37,7 @@ function Decode(fPort, bytes) {
             decoded.peopleCount_1_95 = bytes[i];
             decoded.regionCount_2_95 = bytes[i + 1];
             var region = readUInt16BE(bytes.slice(i + 2, i + 4));
-            for (var idx = 0; idx < decoded.region_count; idx++) {
+            for (var idx = 0; idx < decoded.regionCount_2_95; idx++) {
                 var tmp = "region" + idx + "_" + index++ + "_95";
                 decoded[tmp] = (region > idx) & 1;
             }
@@ -44,13 +45,13 @@ function Decode(fPort, bytes) {
         } 
         // PEOPLE IN/OUT
         else if (channel_id ===0x05 && channel_type === 0xcc) {
-            decoded.peopleIn_101_95  = readInt16LE(bytes.slice(i, i + 2));
+            decoded.peopleIn_101_95 = readInt16LE(bytes.slice(i, i + 2));
             decoded.peopleOut_102_95 = readInt16LE(bytes.slice(i + 2, i + 4));
-            i += 4；
+            i += 4;
         } 
         //PEOPLE MAX
         else if (channel_id ===0x06 && channel_type === 0xcd) {
-            decoded.peopleMax_100_95 = bytes[i];
+             decoded.peopleMax_100_95 = bytes[i];
             i += 1;
         } else {
             break;
@@ -74,4 +75,22 @@ function readInt16LE(bytes) {
 function readUInt16LE(bytes) {
     var value = (bytes[1] << 8) + bytes[0];
     return value & 0xffff;
+}
+
+// bytes to version
+function readVersion(bytes) {
+    var temp = [];
+    for (var idx = 0; idx < bytes.length; idx++) {
+        temp.push((bytes[idx] & 0xff).toString(10));
+    }
+    return temp.join(".");
+}
+
+// bytes to string
+function readString(bytes) {
+    var temp = [];
+    for (var idx = 0; idx < bytes.length; idx++) {
+        temp.push(("0" + (bytes[idx] & 0xff).toString(16)).slice(-2));
+    }
+    return temp.join("");
 }
